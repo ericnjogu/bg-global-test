16903802b640374aebed51f7610eb692380a436e
Eric Njogu <kunadawa@gmail.com>
2020-09-30 18:32:02 +0300

    chore(setup): spring config

---
+++ src/main/kotlin/com/mugowanjogu/banking/AppConfig.kt
@@ -0,0 +1,10 @@
+package com.mugowanjogu.banking
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+
+@Configuration
+@ComponentScan(basePackages = ["com.mugowanjogu.banking.service"])
+@PropertySource("bg-app.properties")
+open class AppConfig
